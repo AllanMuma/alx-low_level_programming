ba3d7fb22dff78fe1359490ccd9d2b10f8b481ec
AllanMuma <allanmumawitty@gmail.com>
2022-03-18 05:20:31 -0700

    test

---
+++ 0-main.c
@@ -0,0 +1,17 @@
+#include "main."
+
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+
+positive integer_or_negative(i);
+return (0);
+}
+
